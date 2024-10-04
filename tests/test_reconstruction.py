d398604d88652aae4e102718356ffcbf07c40ce2
Antonio Senra Filho <acsenrafilho@gmail.com>
2024-10-04 16:00:51 -0300

    ENH: Added test for MultiTE_ASLMapping class

--- tests/test_reconstruction.py
+++ tests/test_reconstruction.py
@@ -5,7 +5,7 @@ import numpy as np
 import pytest
 
 from asltk.asldata import ASLData
-from asltk.reconstruction import CBFMapping
+from asltk.reconstruction import CBFMapping, MultiTE_ASLMapping
 from asltk.utils import load_image
 
 SEP = os.sep
@@ -136,3 +136,103 @@ def test_cbf_map_normalized_flag_true_result_cbf_map_rescaled():
     out['cbf_norm'][out['cbf_norm'] == 0] = np.nan
     mean_px_value = np.nanmean(out['cbf_norm'])
     assert mean_px_value < 500 and mean_px_value > 50
+
+
+def test_multite_asl_object_constructor_created_sucessfully():
+    mte = MultiTE_ASLMapping(asldata)
+    assert isinstance(mte._asl_data, ASLData)
+    assert isinstance(mte._basic_maps, CBFMapping)
+    assert isinstance(mte._brain_mask, np.ndarray)
+    assert isinstance(mte._cbf_map, np.ndarray)
+    assert isinstance(mte._att_map, np.ndarray)
+    assert isinstance(mte._t1blgm_map, np.ndarray)
+
+
+def test_multite_asl_set_brain_mask_success():
+    mte = MultiTE_ASLMapping(asldata)
+    mask = load_image(M0_BRAIN_MASK)
+    mte.set_brain_mask(mask)
+    assert isinstance(mte._brain_mask, np.ndarray)
+
+
+def test_multite_asl_set_cbf_map_success():
+    mte = MultiTE_ASLMapping(asldata)
+    fake_cbf = np.ones((10, 10)) * 20
+    mte.set_cbf_map(fake_cbf)
+    assert np.mean(mte._cbf_map) == 20
+
+
+def test_multite_asl_get_cbf_map_success():
+    mte = MultiTE_ASLMapping(asldata)
+    fake_cbf = np.ones((10, 10)) * 20
+    mte.set_cbf_map(fake_cbf)
+    assert np.mean(mte.get_cbf_map()) == 20
+
+
+def test_multite_asl_set_att_map_success():
+    mte = MultiTE_ASLMapping(asldata)
+    fake_att = np.ones((10, 10)) * 20
+    mte.set_att_map(fake_att)
+    assert np.mean(mte._att_map) == 20
+
+
+def test_multite_asl_get_att_map_success():
+    mte = MultiTE_ASLMapping(asldata)
+    fake_att = np.ones((10, 10)) * 20
+    mte.set_att_map(fake_att)
+    assert np.mean(mte.get_att_map()) == 20
+
+
+@pytest.mark.parametrize('label', [(3), (-1), (1000000), (-1.1), (2.1)])
+def test_multite_asl_set_brain_mask_set_label_value_raise_error_value_not_found_in_mask(
+    label,
+):
+    mte = MultiTE_ASLMapping(asldata)
+    mask = load_image(M0_BRAIN_MASK)
+    with pytest.raises(Exception) as e:
+        mte.set_brain_mask(mask, label=label)
+    assert e.value.args[0] == 'Label value is not found in the mask provided.'
+
+
+def test_multite_asl_set_brain_mask_verify_if_input_is_a_label_mask():
+    mte = MultiTE_ASLMapping(asldata)
+    not_mask = load_image(M0)
+    with pytest.warns(UserWarning):
+        mte.set_brain_mask(not_mask / np.max(not_mask))
+        warnings.warn(
+            'Mask image is not a binary image. Any value > 0 will be assumed as brain label.',
+            UserWarning,
+        )
+
+
+def test_multite_asl_set_brain_mask_raise_error_if_image_dimension_is_different_from_3d_volume():
+    mte = MultiTE_ASLMapping(asldata)
+    pcasl_3d_vol = load_image(PCASL)[0, 0, :, :, :]
+    fake_mask = np.array(((1, 1, 1), (0, 1, 0)))
+    with pytest.raises(Exception) as error:
+        mte.set_brain_mask(fake_mask)
+    assert (
+        error.value.args[0]
+        == f'Image mask dimension does not match with input 3D volume. Mask shape {fake_mask.shape} not equal to {pcasl_3d_vol.shape}'
+    )
+
+
+def test_multite_asl_object_create_map_success():
+    mte = MultiTE_ASLMapping(asldata)
+    out = mte.create_map()
+    assert isinstance(out['cbf'], np.ndarray)
+    assert np.mean(out['cbf']) < 0.0001
+    assert isinstance(out['att'], np.ndarray)
+    assert np.mean(out['att']) > 10
+    assert isinstance(out['t1blgm'], np.ndarray)
+    assert np.mean(out['t1blgm']) > 50
+
+
+def test_multite_asl_object_raises_error_if_asldata_does_not_have_pcasl_or_m0_image():
+    with pytest.raises(Exception) as error:
+        mte = MultiTE_ASLMapping(incomplete_asldata)
+
+    assert (
+        error.value.args[0]
+        == 'ASLData is incomplete. CBFMapping need pcasl and m0 images.'
+    )
