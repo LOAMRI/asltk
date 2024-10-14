fc9e6e604bf3998b652f025c706d30289ebd9d49
Antonio Senra Filho <acsenrafilho@gmail.com>
2024-10-14 17:52:38 -0300

    ENH: New MultiDW_ASLMapping class

--- asltk/reconstruction.py
+++ asltk/reconstruction.py
@@ -8,10 +8,13 @@ from scipy.optimize import curve_fit
 
 from asltk.asldata import ASLData
 from asltk.mri_parameters import MRIParameters
-from asltk.utils import asl_model_buxton, asl_model_multi_te
+from asltk.utils import (
+    asl_model_buxton,
+    asl_model_multi_dw,
+    asl_model_multi_te,
+)
 
 # TODO Opcao para aplicar filtro no mapa de saida (Gauss, etc)
-# TODO Brain mask como input ou opção de processamento?
 
 
 class CBFMapping(MRIParameters):
@@ -27,7 +30,7 @@ class CBFMapping(MRIParameters):
         Examples:
             The default MRIParameters are used as default in the object
             constructor
-            >>> asl_data = ASLData(pcasl='./tests/files/pcasl.nii.gz',m0='./tests/files/m0.nii.gz')
+            >>> asl_data = ASLData(pcasl='./tests/files/pcasl_mte.nii.gz',m0='./tests/files/m0.nii.gz')
             >>> cbf = CBFMapping(asl_data)
             >>> cbf.get_constant('T1csf')
             1400.0
@@ -71,8 +74,7 @@ class CBFMapping(MRIParameters):
         Args:
             brain_mask (np.ndarray): The image representing the brain mask label (int, optional): The label value used to define the foreground tissue (brain). Defaults to 1.
         """
-        # TODO Add flag to create new mask using fsl.bet
-        self._check_mask_values(brain_mask, label)
+        _check_mask_values(brain_mask, label, self._asl_data('m0').shape)
 
         binary_mask = (brain_mask == label).astype(np.uint8) * label
         self._brain_mask = binary_mask
@@ -167,36 +169,6 @@ class CBFMapping(MRIParameters):
             'att': self._att_map,
         }
 
-    def _check_mask_values(self, mask, label):
-        # Check wheter mask input is an numpy array
-        if not isinstance(mask, np.ndarray):
-            raise TypeError(f'mask is not an numpy array. Type {type(mask)}')
-
-        # Check whether the mask provided is a binary image
-        unique_values = np.unique(mask)
-        if unique_values.size > 2:
-            warnings.warn(
-                'Mask image is not a binary image. Any value > 0 will be assumed as brain label.',
-                UserWarning,
-            )
-
-        # Check whether the label value is found in the mask image
-        label_ok = False
-        for value in unique_values:
-            if label == value:
-                label_ok = True
-                break
-        if not label_ok:
-            raise ValueError('Label value is not found in the mask provided.')
-
-        # Check whether the dimensions between mask and input volume matches
-        mask_shape = mask.shape
-        input_vol_shape = self._asl_data('pcasl')[0, 0, :, :, :].shape
-        if mask_shape != input_vol_shape:
-            raise TypeError(
-                f'Image mask dimension does not match with input 3D volume. Mask shape {mask_shape} not equal to {input_vol_shape}'
-            )
-
 
 class MultiTE_ASLMapping(MRIParameters):
     def __init__(self, asl_data: ASLData) -> None:
@@ -212,7 +184,7 @@ class MultiTE_ASLMapping(MRIParameters):
         Examples:
             The default MRIParameters are used as default in the object
             constructor
-            >>> asl_data = ASLData(pcasl='./tests/files/pcasl.nii.gz',m0='./tests/files/m0.nii.gz', te_values=[13.2, 25.7, 50.4])
+            >>> asl_data = ASLData(pcasl='./tests/files/pcasl_mte.nii.gz',m0='./tests/files/m0.nii.gz', te_values=[13.2, 25.7, 50.4])
             >>> mte = MultiTE_ASLMapping(asl_data)
             >>> mte.get_constant('T1csf')
             1400.0
@@ -261,8 +233,7 @@ class MultiTE_ASLMapping(MRIParameters):
         Args:
             brain_mask (np.ndarray): The image representing the brain mask label (int, optional): The label value used to define the foreground tissue (brain). Defaults to 1.
         """
-        # TODO Add flag to create new mask using fsl.bet
-        self._check_mask_values(brain_mask, label)
+        _check_mask_values(brain_mask, label, self._asl_data('m0').shape)
 
         binary_mask = (brain_mask == label).astype(np.uint8) * label
         self._brain_mask = binary_mask
@@ -312,36 +283,6 @@ class MultiTE_ASLMapping(MRIParameters):
             (np.ndarray): _description_
         """
         return self._att_map
-
-    def _check_mask_values(self, mask, label):
-        # Check wheter mask input is an numpy array
-        if not isinstance(mask, np.ndarray):
-            raise TypeError(f'mask is not an numpy array. Type {type(mask)}')
-
-        # Check whether the mask provided is a binary image
-        unique_values = np.unique(mask)
-        if unique_values.size > 2:
-            warnings.warn(
-                'Mask image is not a binary image. Any value > 0 will be assumed as brain label.',
-                UserWarning,
-            )
-
-        # Check whether the label value is found in the mask image
-        label_ok = False
-        for value in unique_values:
-            if label == value:
-                label_ok = True
-                break
-        if not label_ok:
-            raise ValueError('Label value is not found in the mask provided.')
-
-        # Check whether the dimensions between mask and input volume matches
-        mask_shape = mask.shape
-        input_vol_shape = self._asl_data('pcasl')[0, 0, :, :, :].shape
-        if mask_shape != input_vol_shape:
-            raise TypeError(
-                f'Image mask dimension does not match with input 3D volume. Mask shape {mask_shape} not equal to {input_vol_shape}'
-            )
 
     def create_map(
         self,
@@ -422,7 +363,6 @@ class MultiTE_ASLMapping(MRIParameters):
                                 self.T2gm,
                             )
 
-                        # Ydata = np.swapaxes(asl_img[:,:,k,j,i],0,1).reshape((len(pld) * len(te), 1), order='F').flatten('F')
                         Ydata = (
                             self._asl_data('pcasl')[:, :, k, j, i]
                             .reshape(
@@ -485,3 +425,276 @@ class MultiTE_ASLMapping(MRIParameters):
         img = thr_filter.Execute(img)
 
         return sitk.GetArrayFromImage(img)
+
+
+class MultiDW_ASLMapping(MRIParameters):
+    def __init__(self, asl_data: ASLData):
+        super().__init__()
+        self._asl_data = asl_data
+        self._basic_maps = CBFMapping(asl_data)
+        if self._asl_data.get_dw() is None:
+            raise ValueError(
+                'ASLData is incomplete. MultiDW_ASLMapping need a list of DW values.'
+            )
+
+        self._brain_mask = np.ones(self._asl_data('m0').shape)
+        self._cbf_map = np.zeros(self._asl_data('m0').shape)
+        self._att_map = np.zeros(self._asl_data('m0').shape)
+
+        self._b_values = self._asl_data.get_dw()
+        # self._A1 = np.zeros(tuple([len(self._b_values)]) + self._asl_data('m0').shape)
+        self._A1 = np.zeros(self._asl_data('m0').shape)
+        # self._D1 = np.zeros(tuple([1]) +self._asl_data('m0').shape)
+        self._D1 = np.zeros(self._asl_data('m0').shape)
+        self._A2 = np.zeros(self._asl_data('m0').shape)
+        # self._A2 = np.zeros(tuple([len(self._b_values)])  + self._asl_data('m0').shape)
+        # self._D2 = np.zeros(tuple([1]) +self._asl_data('m0').shape)
+        self._D2 = np.zeros(self._asl_data('m0').shape)
+        self._kw = np.zeros(self._asl_data('m0').shape)
+
+    def set_brain_mask(self, brain_mask: np.ndarray, label: int = 1):
+        """Defines whether a brain a mask is applied to the MultiDW_ASLMapping
+        calculation
+
+        A image mask is simply an image that defines the voxels where the ASL
+        calculation should be made. Basically any integer value can be used as
+        proper label mask.
+
+        A most common approach is to use a binary image (zeros for background
+        and 1 for the brain tissues). Anyway, the default behavior of the
+        method can transform a integer-pixel values image to a binary mask with
+        the `label` parameter provided by the user
+
+        Args:
+            brain_mask (np.ndarray): The image representing the brain mask label (int, optional): The label value used to define the foreground tissue (brain). Defaults to 1.
+        """
+        _check_mask_values(brain_mask, label, self._asl_data('m0').shape)
+
+        binary_mask = (brain_mask == label).astype(np.uint8) * label
+        self._brain_mask = binary_mask
+
+    def get_brain_mask(self):
+        """Get the brain mask image
+
+        Returns:
+            (np.ndarray): The brain mask image
+        """
+        return self._brain_mask
+
+    def set_cbf_map(self, cbf_map: np.ndarray):
+        """Set the CBF map to the MultiDW_ASLMapping object.
+
+        Note:
+            The CBF maps must have the original scale in order to calculate the
+            T1blGM map correclty. Hence, if the CBF map was made using
+            CBFMapping class, one can use the 'cbf' output.
+
+        Args:
+            cbf_map (np.ndarray): The CBF map that is set in the MultiDW_ASLMapping object
+        """
+        self._cbf_map = cbf_map
+
+    def get_cbf_map(self) -> np.ndarray:
+        """Get the CBF map storaged at the MultiDW_ASLMapping object
+
+        Returns:
+            (np.ndarray): The CBF map that is storaged in the
+            MultiDW_ASLMapping object
+        """
+        return self._cbf_map
+
+    def set_att_map(self, att_map: np.ndarray):
+        """Set the ATT map to the MultiDW_ASLMapping object.
+
+        Args:
+            att_map (np.ndarray): The ATT map that is set in the MultiDW_ASLMapping object
+        """
+        self._att_map = att_map
+
+    def get_att_map(self):
+        """Get the ATT map storaged at the MultiDW_ASLMapping object
+
+        Returns:
+            (np.ndarray): _description_
+        """
+        return self._att_map
+
+    def create_map(
+        self,
+        lb: list = [0.0, 0.0, 0.0, 0.0],
+        ub: list = [np.inf, np.inf, np.inf, np.inf],
+        par0: list = [0.5, 0.000005, 0.5, 0.000005],
+    ):
+        self._basic_maps.set_brain_mask(self._brain_mask)
+
+        basic_maps = {'cbf': self._cbf_map, 'att': self._att_map}
+        if np.mean(self._cbf_map) == 0 or np.mean(self._att_map) == 0:
+            # If the CBF/ATT maps are zero (empty), then a new one is created
+            print(
+                '[blue][INFO] The CBF/ATT map were not provided. Creating these maps before next step...'
+            )
+            basic_maps = self._basic_maps.create_map()
+            self._cbf_map = basic_maps['cbf']
+            self._att_map = basic_maps['att']
+
+        x_axis = self._asl_data('m0').shape[2]   # height
+        y_axis = self._asl_data('m0').shape[1]   # width
+        z_axis = self._asl_data('m0').shape[0]   # depth
+
+        for i in track(
+            range(x_axis), description='[green]multiDW-ASL processing...'
+        ):
+            for j in range(y_axis):
+                for k in range(z_axis):
+                    if self._brain_mask[k, j, i] != 0:
+                        # Calculates the diffusion components for (A1, D1), (A2, D2)
+                        def mod_diff(Xdata, par1, par2, par3, par4):
+                            return asl_model_multi_dw(
+                                b_values=Xdata,
+                                A1=par1,
+                                D1=par2,
+                                A2=par3,
+                                D2=par4,
+                            )
+
+                        # M(t,b)/M(t,0)
+                        Ydata = (
+                            self._asl_data('pcasl')[:, :, k, j, i]
+                            .reshape(
+                                (
+                                    len(self._asl_data.get_ld())
+                                    * len(self._asl_data.get_dw()),
+                                    1,
+                                )
+                            )
+                            .flatten()
+                            / self._asl_data('m0')[k, j, i]
+                        )
+
+                        try:
+                            # Xdata = self._b_values
+                            Xdata = self._create_x_data(
+                                self._asl_data.get_ld(),
+                                self._asl_data.get_pld(),
+                                self._asl_data.get_dw(),
+                            )
+
+                            par_fit, _ = curve_fit(
+                                mod_diff,
+                                Xdata[:, 2],
+                                Ydata,
+                                p0=par0,
+                                bounds=(lb, ub),
+                            )
+                            self._A1[k, j, i] = par_fit[0]
+                            self._D1[k, j, i] = par_fit[1]
+                            self._A2[k, j, i] = par_fit[2]
+                            self._D2[k, j, i] = par_fit[3]
+                        except RuntimeError:
+                            self._A1[k, j, i] = 0
+                            self._D1[k, j, i] = 0
+                            self._A2[k, j, i] = 0
+                            self._D2[k, j, i] = 0
+
+                        # Calculates the Mc fitting to alpha = kw + T1blood
+                        m0_px = self._asl_data('m0')[k, j, i]
+
+                        # def mod_2comp(Xdata, par1):
+                        #     ...
+                        #     # return asl_model_multi_te(
+                        #     #     Xdata[:, 0],
+                        #     #     Xdata[:, 1],
+                        #     #     Xdata[:, 2],
+                        #     #     m0_px,
+                        #     #     basic_maps['cbf'][k, j, i],
+                        #     #     basic_maps['att'][k, j, i],
+                        #     #     par1,
+                        #     #     self.T2bl,
+                        #     #     self.T2gm,
+                        #     # )
+
+                        # Ydata = (
+                        #     self._asl_data('pcasl')[:, :, k, j, i]
+                        #     .reshape(
+                        #         (
+                        #             len(self._asl_data.get_ld())
+                        #             * len(self._asl_data.get_te()),
+                        #             1,
+                        #         )
+                        #     )
+                        #     .flatten()
+                        # )
+
+                        # try:
+                        #     Xdata = self._create_x_data(
+                        #         self._asl_data.get_ld(),
+                        #         self._asl_data.get_pld(),
+                        #         self._asl_data.get_dw(),
+                        #     )
+                        #     par_fit, _ = curve_fit(
+                        #         mod_2comp,
+                        #         Xdata,
+                        #         Ydata,
+                        #         p0=par0,
+                        #         bounds=(lb, ub),
+                        #     )
+                        #     self._kw[k, j, i] = par_fit[0]
+                        # except RuntimeError:
+                        #     self._kw[k, j, i] = 0.0
+
+        # # Adjusting output image boundaries
+        # self._kw = self._adjust_image_limits(self._kw, par0[0])
+
+        return {
+            'cbf': self._cbf_map,
+            'cbf_norm': self._cbf_map * (60 * 60 * 1000),
+            'att': self._att_map,
+            'a1': self._A1,
+            'd1': self._D1,
+            'a2': self._A2,
+            'd2': self._D2,
+            'kw': self._kw,
+        }
+
+    def _create_x_data(self, ld, pld, dw):
+        # array for the x values, assuming an arbitrary size based on the PLD
+        # and TE vector size
+        Xdata = np.zeros((len(pld) * len(dw), 3))
+
+        count = 0
+        for i in range(len(pld)):
+            for j in range(len(dw)):
+                Xdata[count] = [ld[i], pld[i], dw[j]]
+                count += 1
+
+        return Xdata
+
+
+def _check_mask_values(mask, label, ref_shape):
+    # Check wheter mask input is an numpy array
+    if not isinstance(mask, np.ndarray):
+        raise TypeError(f'mask is not an numpy array. Type {type(mask)}')
+
+    # Check whether the mask provided is a binary image
+    unique_values = np.unique(mask)
+    if unique_values.size > 2:
+        warnings.warn(
+            'Mask image is not a binary image. Any value > 0 will be assumed as brain label.',
+            UserWarning,
+        )
+
+    # Check whether the label value is found in the mask image
+    label_ok = False
+    for value in unique_values:
+        if label == value:
+            label_ok = True
+            break
+    if not label_ok:
+        raise ValueError('Label value is not found in the mask provided.')
+
+    # Check whether the dimensions between mask and input volume matches
+    mask_shape = mask.shape
+    if mask_shape != ref_shape:
+        raise TypeError(
+            f'Image mask dimension does not match with input 3D volume. Mask shape {mask_shape} not equal to {ref_shape}'
+        )
