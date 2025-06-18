d1a6e94b6560fb320f5a975fd38bc369f2745d2c
acsenrafilho <acsenrafilho@gmail.com>
2025-06-18 10:30:14 -0300

    WIP: Parcellation Report partial implementation

--- asltk/__init__.py
+++ asltk/__init__.py
@@ -1,2 +1,6 @@
+import os
+
 BIDS_IMAGE_FORMATS = ('.nii', '.nii.gz')
 AVAILABLE_IMAGE_FORMATS = ('.nii', '.nii.gz', '.mha', '.nrrd')
+
+PARCELLATION_REPORT_PATH = os.path.join(os.path.expanduser('~'), 'asltk',os.path.sep,'parcellation_reports')
