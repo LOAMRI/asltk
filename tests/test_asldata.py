e28af49c3fcea98e8c5a54514505f09c3e5cc9aa
acsenrafilho <acsenrafilho@gmail.com>
2025-07-30 18:32:01 -0300

    STY: Fix linter

--- tests/test_asldata.py
+++ tests/test_asldata.py
@@ -3,15 +3,8 @@ import os
 import numpy as np
 import pytest
 
-from asltk.utils import load_image
-
-from asltk.utils import load_image
-
-from asltk.utils import load_image
-
-from asltk.utils import load_image
-
 from asltk import asldata
+from asltk.utils import load_image
 from asltk.utils.io import load_image, save_image
 
 SEP = os.sep
@@ -97,11 +90,13 @@ def test_create_object_check_initial_parameters():
     assert obj.get_ld() == []
     assert obj.get_pld() == []
 
+
 def test_create_object_with_m0_as_numpy_array():
     array = load_image(M0)
     obj = asldata.ASLData(m0=array)
 
     assert obj('m0').shape == array.shape
+
 
 def test_create_object_with_m0_as_numpy_array():
     array = load_image(M0)
@@ -115,6 +110,7 @@ def test_create_object_with_pcasl_as_numpy_array():
     obj = asldata.ASLData(pcasl=array)
 
     assert obj('pcasl').shape == array.shape
+
 
 def test_get_ld_show_empty_list_for_new_object():
     obj = asldata.ASLData()
